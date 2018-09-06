10ec02bda72a694819efa31fe97735ed90b1f93d
zoezoezy <42957706+zoezoezy@users.noreply.github.com>
2018-09-06 13:33:34 +0800

    Add files via upload

---
+++ 002Console/alert.js
@@ -0,0 +1,20 @@
+		/*function woDeTaoLu() {
+			alert("taolu1=====你想看看啥子！");
+			alert("taolu2=====我就是不给你看！");
+			alert("taolu3=====那你还想看不！");	
+		}
+		
+		alert("1====子曰：\nzoe是天才！");  <!-- \n 转行-->
+		alert("你怎么打开了这个网页");
+		
+		woDeTaoLu();  <!--调用taolu()函数-->
+		alert("你相信奇迹么？");
+		alert("让我来向你提个问题");
+		
+		woDeTaoLu();
+		alert("果然没有奇迹");
+		alert("我们就这样放弃吗？");
+		
+		woDeTaoLu();*/
+		
+		alert("2====是不是不想看啦,88！");
